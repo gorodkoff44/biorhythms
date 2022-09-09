75a3dbc4acdb2243e3bdd4b95aa25b7d0a2c2e8f
dennydze <dannay.egorov@gmail.com>
2022-09-09 11:35:10 +0300

    Ограничения
    Добавил ограничения ввода

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,8 +22,13 @@ namespace Биоритмы
     /// 
     public partial class MainWindow : Window
     {
-        int z; //длительность прогноза
-        private double B1, B2, B3, SUM, t, pi = 3.14; //Из формулы
+        private int z; //длительность прогноза
+        private double B1;
+        private double B2;
+        private double B3;
+        private double SUM;
+        private double t;
+        private readonly double pi = 3.14;
         private DateTime DATA; //дата в таблицу
 
         private void Clean_Click(object sender, RoutedEventArgs e) //очистка таблицы
@@ -36,6 +41,7 @@ namespace Биоритмы
         public MainWindow()
         {
             InitializeComponent();
+            ProizvProg.MaxLength = 6;
         }
         private void Pvd_Click(object sender, RoutedEventArgs e) //Для чекбокса длительность прогноза
         {
@@ -52,37 +58,56 @@ namespace Биоритмы
         }
         public void Ok_Click(object sender, RoutedEventArgs e)
         {
+            
             List<BioTable> result = new List<BioTable>(3); //хранит данные
             if ((ComDlit.Text == "" && pvd.IsChecked == false) || (ProizvProg.Text == "" && pvd.IsChecked == true)) //проверка на пустое поле
             {
                 MessageBox.Show("Введите длительность прогноза", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                ProizvProg.Text = "";
                 return;
             }
-            if (pvd.IsChecked == true)
-                z = int.Parse(ProizvProg.Text);
-            else
-                z = int.Parse(ComDlit.Text);
+            if (pvd.IsChecked == false)
+            {
+                try
+                {
+                    int.Parse(ProizvProg.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("Введите длительность прогноза числом", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    ProizvProg.Text = "";
+                    return;
+                }
+                if (int.Parse(ProizvProg.Text) < 0)
+                {
+                    MessageBox.Show("Длительность прогноза не может быть отрицательной", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    ProizvProg.Text = "";
+                    return;
+                }
+            }
+
+            z = pvd.IsChecked == true ? int.Parse(ProizvProg.Text) : int.Parse(ComDlit.Text);
             Stata.Text = "Дата рождения:\n" + SelDRCom.Text + "\nДлительность прогноза: " + z + "\nПериод с " + DO.Text + " по " + DateTime.Today.ToString("D");
             DateTime a = DateTime.Parse(SelDRCom.Text);  //перевод выбранной даты в datetime
             DateTime b = DateTime.Parse(DateTime.Today.ToString()); //перевод даты на сегодня в datetime
             DateTime dto = DateTime.Parse(DO.Text.ToString());
             int tt;
             t = (b - a).TotalDays;//количество дней с др
-            
+
             tt = Convert.ToInt32((dto - a).TotalDays); //к-во дней с др до даты отчета
-            Console.WriteLine($"к-во дней с др до даты отчета {tt}");
+                                                       //Console.WriteLine($"к-во дней с др до даты отчета {tt}");
             t -= tt;
             //z += tt;
             for (int i = 0; i < z; i++)
             {
                 t += i;
                 //Console.WriteLine(t);
-                B1 = Math.Round((Math.Sin(2 * pi * t / 23 * 100)),2);
+                B1 = Math.Round((Math.Sin(2 * pi * t / 23 * 100)), 2);
                 Console.WriteLine($"(2 * {pi} * {t} / 23 * 100);");
                 B2 = Math.Round(Math.Sin(2 * pi * t / 28 * 100), 2);
                 B3 = Math.Round(Math.Sin(2 * pi * t / 33 * 100), 2);
                 SUM = Math.Round(B1 + B2 + B3);
-                DATA = a.AddDays(i+tt);
+                DATA = a.AddDays(i + tt);
                 result.Add(new BioTable(1, B1, B2, B3, SUM, DATA));
                 //Console.WriteLine($"{B1} {B2} {B3}\n");
             }
@@ -90,4 +115,5 @@ namespace Биоритмы
         }
     }
 }
+
 
